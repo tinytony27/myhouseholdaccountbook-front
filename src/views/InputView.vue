03e35f46e5bb7cf778fcc19b00d5f2e0bb4bd0c2
tony <33270318+tinytony27@users.noreply.github.com>
2023-05-27 13:23:33 +0900

    一旦コミット

--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // import HelloWorld from '@/components/HelloWorld.vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
@@ -10,16 +10,35 @@ const processingFlag = ref<boolean>(false);
 const processingTime = 300;
 const itemInput = ref<string>('');
 const editItemID = ref<number>(0);
+
+// const inputs = ref([]);
+// onMounted(() => {
+//   for(let i = 0; i < store.state.inputList.length; i++){
+//     const tmp = '' as never;
+//     inputs.value.push(tmp);
+//   }
+// });
+const test1 = ref();
+const onEnterKeyUp = () => {
+  console.log('pushed!');
+  test1.value.focus();
+  // console.log(inputs.value[0]);
+  // if (index < inputs.value.length - 1) {
+  //   inputs.value[index + 1].focus();
+  // }
+};
 </script>
 
 <template>
   <div class="h-full bg-gray-50">
     <div class="relative h-full w-full pt-1 pb-28 overflow-y-scroll">
+      <input @keydown.enter="onEnterKeyUp()" autofocus />
+      <input ref="test" @keydown.enter="onEnterKeyUp()" />
       <div v-for="(elem, index) in store.state.inputList" :key="index" class="w-full">
         <div class="bg-red-100 h-16 mb-1">
-          <!-- <label>
+          <label>
             <span></span>
-            <input type="text" placeholder=" MM / DD "/>
+            <input type="text" placeholder=" MM / DD " ref="test1" @keyup.enter="onEnterKeyUp()"/>
           </label>
           <label>
             <select name="pets" id="pet-select">
@@ -31,7 +50,7 @@ const editItemID = ref<number>(0);
           <label>
             <span></span>
             <input type="number" placeholder="金額"/>
-          </label> -->
+          </label>
         </div>
       </div>
     </div>
